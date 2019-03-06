16785fbde97ff677f258e347eaf1b3b127440bec
Kartik Kumar Gujarati <kartikkumar.gujarati@rackspace.com>
2019-03-05 22:07:28 -0600

    Refactored to add a drawer layout with two fragments.

---
+++ Networking/app/src/main/java/com/kartik/networking/ui/base/BaseView.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by Kartik Kumar Gujarati on 3/5/19 8:05 AM
+ * Copyright (c) 2019 . All rights reserved.
+ *
+ * Last modified 3/5/19 8:05 AM
+ */
+
+package com.kartik.networking.ui.base
+
+interface BaseView {
+    fun showErrorToast()
+}
